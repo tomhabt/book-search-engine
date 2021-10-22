f2b63d8ab4349cf50f19aa311c560819e0e4c3f2
Thomas Habtemariam <84083304+tomhabt@users.noreply.github.com>
2021-10-22 17:14:21 -0400

    askBCS solution

---
+++ client/build/precache-manifest.c852fa8d534bb8c5bba4e42e09e724c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86ee920173483a08bf5bfe38ed1eb29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "732f4eb167114ff723dd",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "a5d0e438caee00c0331f",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "732f4eb167114ff723dd",
+    "url": "/static/js/2.5e2ba4ae.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.5e2ba4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d0e438caee00c0331f",
+    "url": "/static/js/main.7036eb62.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-});
+})
+.then(() => console.log("MongoDB has been connected"))
+.catch((error) => console.log(err))
 
 module.exports = mongoose.connection;
